8e79b3905642308820e0e794008de5a9a93ccc32
ziahziahziah <168610860+ziahziahziah@users.noreply.github.com>
2025-06-14 17:34:25 -0400

    Add jest setup and test for index

---
+++ __tests__/index.test.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const {JSDOM} = require('jsdom');
+
+// Ensures test runs with navigator.getBattery undefined
+
+test('index.html executes without navigator.getBattery', () => {
+  const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
+  const dom = new JSDOM(html, { runScripts: 'dangerously', resources: 'usable', url: 'http://localhost' });
+  // Accessing document triggers script execution synchronously
+  expect(() => dom.window.document).not.toThrow();
+});
